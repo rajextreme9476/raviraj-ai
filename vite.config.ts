b884ec0979c0cfc7b7e397868a61a93b1b983310
Raviraj Desai <ravirajdesai501@gmail.com>
2025-07-08 10:40:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// vite.config.js
+export default {
+  base: "/raviraj-ai/",
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
